b69aec38281b210114e62368fd042eab903f9919
rflood <rflood@mcad.edu>
2014-03-01 10:45:31 -0800

    repeater loop edit in content-page.php

--- content-page.php
+++ content-page.php
@@ -6,11 +6,34 @@
  */
 ?>
 
+<?php
+
+// check if the repeater field has rows of data
+if( have_rows('home_page_section') ):
+
+    // loop through the rows of data
+    while ( have_rows('home_page_section') ) : the_row();
+
+        // display a sub field value
+        the_sub_field('upper_content');
+        the_sub_field('lower_content');
+        the_sub_field('background_image');
+
+    endwhile;
+
+else :
+
+    // no rows found
+
+endif;
+
+?>
+
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
-
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
@@ -20,5 +43,6 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
+
 	<!-- <?php edit_post_link( __( 'Edit', 'sanctuary' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?> -->
 </article><!-- #post-## -->
