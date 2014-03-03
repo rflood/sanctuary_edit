dbacbca1db4dc823a32097f1d426c8a44adb3894
rflood <rflood@mcad.edu>
2014-03-02 20:27:54 -0800

    js/flexslider.js js/main.js

--- functions.php
+++ functions.php
@@ -89,7 +89,10 @@ function sanctuary_scripts() {
 
     wp_enqueue_script( 'parallax', get_template_directory_uri() . '/js/parallax.js', array(), '1.0.0', true );
 
-    wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/js/flexslider.js', array(), '1.0.0', true );
+    wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', true );
+
+    wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/js/jquery.flexslider.js', array(), '1.0.0', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
